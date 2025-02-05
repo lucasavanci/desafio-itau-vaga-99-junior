14483e306a016a5fa84c8ed53b287f57c49453ea
Lucas Avanci <lucasavanci98@gmail.com>
2025-02-05 11:56:36 -0300

    Primeiro Commit - Spring Initializr

---
+++ transacoes-estatisticas/src/main/java/com/desafioitau/transacoes_estatisticas/TransacoesEstatisticasApplication.java
@@ -0,0 +1,13 @@
+package com.desafioitau.transacoes_estatisticas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransacoesEstatisticasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransacoesEstatisticasApplication.class, args);
+	}
+
+}
---
+++ transacoes-estatisticas/src/test/java/com/desafioitau/transacoes_estatisticas/TransacoesEstatisticasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafioitau.transacoes_estatisticas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransacoesEstatisticasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
